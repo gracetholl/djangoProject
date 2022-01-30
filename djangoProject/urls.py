1dc3c2e3c84a358a7ca4f18c4f6855fd319899bb
m-qu <m.quinn@ufl.edu>
2022-01-29 21:05:10 -0500

    updated database with authors and links

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("projects/", include("projects.urls")),
+    path("home/", include("projects.urls")),
 
 ]
