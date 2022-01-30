fddb76024a1e091c18833995ef39dbee84f07dd2
gracetholl <gracetholl@ufl.edu>
2022-01-29 19:21:47 -0500

    Update

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', TemplateView.as_view(template_name="home.html")),
-    path('', include('hello_world.urls')),
+    path("projects/", include("projects.urls")),
 
 ]
